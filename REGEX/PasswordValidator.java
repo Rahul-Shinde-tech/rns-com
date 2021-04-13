504b5a914864af581766a38227b2ed7e478b8c1d
Rahul-Shinde-tech <rns080390@gmail.com>
2021-04-13 21:15:04 +0530

    com.rns.REGEX/1304212114

---
+++ REGEX/PasswordValidator.java
@@ -0,0 +1,27 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PasswordValidator {
+
+    private static final String PASSWORD_PATTERN = "(^(?=.[0-9])(?=.[a-z])(?=.[A-Z])(?=.[@#$%^&+=])(?=\\S+$).{8,}$)";  //d = [0-9]
+
+    public static PasswordValidator(){
+        pattern = Pattern.compile(PASSWORD_PATTERN);
+    }
+
+    public boolean validate(final String password) {
+
+        matcher = pattern.matcher(password);
+        return matcher.matches();
+
+    }
+
+    public static void main(String[] args) {
+
+        String inputPassword = "J@y!67tj";
+
+        PasswordValidator passwordValidator = new PasswordValidator();
+        System.out.println(passwordValidator.validate(inputPassword));
+
+    }
+}
